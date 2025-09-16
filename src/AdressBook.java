d947dc9150ae797ed39366cc8be2dfc64f04dd66
gabrielbugarija <Gabriel.bugarija@outlook.com>
2025-09-16 13:31:57 -0400

    Added a print statement.

---
+++ src/AdressBook.java
@@ -0,0 +1,16 @@
+public class AdressBook {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Adress Book");
+    }
+    public void addBuddy(){
+
+    }
+
+    public void removeBuddy(){
+
+
+    }
+}
